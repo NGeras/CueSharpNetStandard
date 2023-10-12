2e87caed6dc356fd7de89c057d35efbbaad8701b
Nikita Gerassimov <Nikita.Gerassimov@bait.ee>
2023-10-12 11:51:59 +0300

    Upgrade CueSharp library to NetStandard2.1

--- CueSharp/Properties/AssemblyInfo.cs
+++ CueSharp/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("CueSharp")]
-[assembly: AssemblyDescription("Cue sheet parser library")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("wyDay")]
-[assembly: AssemblyProduct("CueSharp")]
-[assembly: AssemblyCopyright("Copyright © 2005-2007 wyDay")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,13 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0cbbf417-3aa4-4f3a-aa90-e9f7aef735b5")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("0.5.0")]
-[assembly: AssemblyFileVersion("0.5.0")]
--- TestCueSharp/Properties/AssemblyInfo.cs
+++ TestCueSharp/Properties/AssemblyInfo.cs
@@ -1,15 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,13 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("da8fc6db-d569-412c-a2a2-beee2ddbbacf")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
